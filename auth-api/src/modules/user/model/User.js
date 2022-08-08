342dc8d2078e771bcb8d80ea840127824b290864
RafaRato <rafaelcteixeira1@hotmail.com>
2022-08-08 10:45:41 -0300

    organizacao auth-api em multicamadas

--- auth-api/src/modules/user/model/User.js
+++ auth-api/src/modules/user/model/User.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
-import sequelize from "../../../db/dbConfig.js";
+import sequelize from "../../../config/db/dbConfig.js";
 
 const User = sequelize.define(
     "user",
