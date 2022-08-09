39b59d40bd01ae45d7e9c0de8feb73d9dcd6eae8
RafaRato <rafaelcteixeira1@hotmail.com>
2022-08-09 17:11:54 -0300

    autenticacao de usuario

--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -1,6 +1,10 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import UserRepository from "../repository/UserRepository.js"
-import * as httpStatus from "../../../config/constants/httpStatus.js";
 import UserException from "../exception/UserException.js";
+import * as httpStatus from "../../../config/constants/httpStatus.js";
+import * as secrets from "../../../config/constants/secrets.js";
 
 class UserService {
 
@@ -43,6 +47,49 @@ class UserService {
             );
         }
     }
+
+    async getAccessToken(req) {
+        try {
+            const { email, password } = req.body;
+            this.validadeAccessToken(email, password);
+            let user = await UserRepository.findByEmail(email);
+            this.validadeUserNotFound(user);
+            await this.validatePassword(password, user.password);
+            const authUser = {
+                id: user.id,
+                name: user.name,
+                email: user.email
+            };
+            const accessToken = jwt.sign({authUser}, secrets.API_SECRET, {expiresIn: "1d"});
+            return {
+                status: httpStatus.SUCCESS,
+                accessToken,
+            };
+        } catch (err) {
+            return {
+                status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+                message: err.message,
+            } 
+        }
+
+    }
+
+    validadeAccessToken(email, password) {
+        if (!email || !password) {
+            throw new UserException(
+                httpStatus.UNAUTHORIZED,
+                "Email and Password must be informed."
+            );
+        }
+    }
+
+    async validatePassword(password, hashPassword) {
+        if (!await bcrypt.compare(password, hashPassword)) {
+            throw new UserException(httpStatus.UNAUTHORIZED, "Password doesn't match.");
+        }
+    }
+
+
 }
 
 export default new UserService();
