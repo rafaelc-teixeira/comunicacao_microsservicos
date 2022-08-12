934a37e26645408ef82c0c88caae9cd8ec1b5359
RafaRato <rafaelcteixeira1@hotmail.com>
2022-08-12 10:33:06 -0300

    mapeamento inicial das classes category, product e supplier

---
+++ product-api/src/main/java/br/com/microsservices/productapi/modules/product/model/Product.java
@@ -0,0 +1,27 @@
+package br.com.microsservices.productapi.modules.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "PRODUCT")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    @Column(name = "NAME", nullable = false)
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "FK_CATEGORY", nullable = false)
+    private Category category;
+    @ManyToOne
+    @JoinColumn(name = "FK_SUPPLIER", nullable = false)
+    private Supplier supplier;
+}
